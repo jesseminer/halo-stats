8466ee2dcb6727b900a58c058f3f8f441dc7b481
Jesse Miner <jesse@givegab.com>
2015-12-27 00:23:36 -0500

    Start player page

--- app/models/service_record.rb
+++ app/models/service_record.rb
@@ -4,4 +4,12 @@ class ServiceRecord < ActiveRecord::Base
   belongs_to :player
 
   validates :player_id, uniqueness: { scope: :game_mode }
+
+  def kd_ratio
+    kills.to_f / deaths
+  end
+
+  def win_percentage
+    games_won.to_f / games_played * 100
+  end
 end
