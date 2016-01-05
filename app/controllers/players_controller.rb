d392cad307989d53ed980892b5f21a68840042ad
Jesse Miner <jesse@givegab.com>
2016-01-05 00:42:30 -0500

    Add refresh button to profile

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,6 +3,12 @@ class PlayersController < ApplicationController
     @player = Player.find(params[:id])
   end
 
+  def update
+    player = Player.find(params[:id])
+    FetchPlayer.new(player.gamertag).update
+    redirect_to player_path(player)
+  end
+
   def search
     player = Player.find_by_gamertag(params[:gamertag])
     if player.present?
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'landing#show'
 
-  resources :players, only: [:show] do
+  resources :players, only: [:show, :update] do
     collection do
       post :search
     end
