fb8726b8c5dcd79e1e714078e73dcb5f93b189da
Jesse Miner <jesse@givegab.com>
2016-01-13 23:11:54 -0500

    Friendly urls for players

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,9 +2,20 @@ class Player < ActiveRecord::Base
   has_many :playlist_ranks
   has_many :service_records
 
-  validates :gamertag, uniqueness: true
+  validates :gamertag, presence: true, uniqueness: true
+  validates :slug, presence: true, uniqueness: true
+
+  before_validation { self.slug = gamertag.parameterize }
 
   def self.find_by_gamertag(gt)
     find_by('lower(gamertag) = lower(?)', gt)
+  end
+
+  def self.find(id_or_slug)
+    id_or_slug.to_s.match(/^\d+$/) ? find_by!(id: id_or_slug) : find_by!(slug: id_or_slug)
+  end
+
+  def to_param
+    slug
   end
 end
