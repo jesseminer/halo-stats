33dc2e1e4fe4e93b0944a8be6d61d9ecedf26620
Jesse Miner <jesse@givegab.com>
2016-02-13 02:37:42 -0500

    Show warzone weapon stats on profile

--- app/assets/javascripts/weapon_efficiency.js
+++ app/assets/javascripts/weapon_efficiency.js
@@ -1,4 +1,9 @@
 $(function () {
+  $('#select-game-mode').on('change', function () {
+    $('#arena-weapons, #warzone-weapons').addClass('hide');
+    $('#' + $(this).val() + '-weapons').removeClass('hide');
+  });
+
   $('#include-power-weapons').on('change', function () {
     $(this).prop('checked') ?
       $('.weapon-listing').removeClass('hide') :
