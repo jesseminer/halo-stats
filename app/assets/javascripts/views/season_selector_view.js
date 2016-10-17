be6e042d3c24e2909370e7eb10e363dad491f381
Jesse Miner <jesse@givegab.com>
2016-10-16 21:33:26 -0400

    Can switch seasons

--- app/assets/javascripts/views/season_selector_view.js
+++ app/assets/javascripts/views/season_selector_view.js
@@ -1,14 +1,37 @@
 app.SeasonSelectorView = Backbone.View.extend({
+  events: {
+    'change select': 'changeSeason'
+  },
+
   initialize: function () {
     _.bindAll(this, 'render');
   },
 
-  loadRanks: function () {
-    $.getJSON('/playlist_ranks', { player_id: this.model.get('id') }).done(this.render);
+  changeSeason: function (e) {
+    this.filterBySeason(parseInt($(e.target).val())).renderRanks();
   },
 
-  render: function (data) {
-    this.$el.html(app.template('players/playlist_ranks', data));
+  filterBySeason: function (seasonId) {
+    this.playlistRanks = _.filter(this.allRanks, { season_id: seasonId });
     return this;
+  },
+
+  loadRanks: function () {
+    var view = this;
+    $.getJSON('/playlist_ranks', { player_id: this.model.get('id') }).done(function (data) {
+      view.allRanks = data.playlist_ranks;
+      view.seasons = data.seasons;
+      view.filterBySeason(data.seasons[0].id).render();
+    });
+  },
+
+  render: function () {
+    this.$el.html(app.template('players/season_selector', { seasons: this.seasons }));
+    this.renderRanks();
+    return this;
+  },
+
+  renderRanks: function () {
+    this.$('.ranks-wrapper').html(app.template('players/playlist_ranks', { playlist_ranks: this.playlistRanks }));
   }
 });
